c82a1e1ecab933af41fdc8678e57ac78740cf752
Naoufal271 <naoufalmouahhidi236@gmail.com>
2020-12-16 16:32:33 +0100

    Bonjour Boreale

--- 01/programme.py
+++ 01/programme.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 
-@author: CollegeBoreal
+@author: CollegeBoreal/INF1039-202-20A-01
 """
 
 def main():
-  #print('Informatique: le rêve')
+  print('Informatique: le rêve')
 
 if __name__== "__main__":
   main()
+def main():
+  print('Informatique: le rêve')
+if __name__== "__main__":
+    main()
+
+
